11c675a9f9b7c2230438e2f27ae590215bebcb11
Karsten1999 <jespert.karsten@gmail.com>
2022-01-10 12:48:26 +0100

    Changed note picking strategy for Linear regression

--- Code/Linear_regression_fit.py
+++ Code/Linear_regression_fit.py
@@ -1,19 +1,32 @@
 import Preprocessing
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_absolute_error
 import numpy as np
 from matplotlib import pyplot as plt
 
 voice1 = np.loadtxt("F.txt").T[0]
 vector = Preprocessing.Transform_into_vector(voice1)
 score = []
+difference = []
 
 window = range(1,128,1)
 tot = len(window)
 k = 0
 for i in window:
-    X_train, X_test, y_train, y_test = Preprocessing.Split_rolling_window(voice1, vector, window_size=i)
-    reg = LinearRegression().fit(X_train, y_train)
-    score.append(reg.score(X_test, y_test))
+    temp_score = []
+    for j in range(49):
+        X_train, X_test, y_train, y_test = Preprocessing.Split_rolling_window(voice1, vector, window_size=i)
+        reg = LinearRegression().fit(X_train, y_train)
+
+        y_pred = reg.predict(X_test)
+
+        y_prediction = []
+        for yp in y_pred:
+            yp[yp<max(yp)]=0
+            yp[yp==max(yp)]=1
+            y_prediction.append(yp)
+        temp_score.append(r2_score(y_test, y_prediction))
+    score.append(np.mean(temp_score))
     k+=1
     print("Done", k / tot)
 
