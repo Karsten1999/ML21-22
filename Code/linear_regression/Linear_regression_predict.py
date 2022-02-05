d361cb4cb0a82fe32d649f387def63080149f3f6
JesperKarsten <jespert.karsten@gmail.com>
2022-02-05 22:26:27 +0100

    Final linear regression stuff

--- Code/linear_regression/Linear_regression_predict.py
+++ Code/linear_regression/Linear_regression_predict.py
@@ -10,46 +10,50 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 
-
-# data
-voice1 = np.loadtxt("../data/F.txt").T[0][3095::]
-vector = Preprocessing.Transform_into_vector(voice1)
-
-
-# minimum note used for converting probability vector back into note
-min_note = min(voice1[voice1>0])
-
-predicted_voice = []
-
-# Parameter to set length of prediction
-prediction_length = 256
-# Parameter to set windows size
-window_size = 650 # 500,600, 650, 680 was good, 650 best probably
-
-X, y = Preprocessing.Split_rolling_window(voice1, vector, window_size=window_size, train=False)
-
-scaler = StandardScaler()
-scaler.fit(X)
-reg = LinearRegression().fit(scaler.transform(X), y)
+def create_track(tracknumber: int, window_size = 500):
+    # 500,600, 650, 680 was good, 650 best probably for window size
+    # data
+    voice1 = np.loadtxt("../data/F.txt").T[tracknumber-1][3095::]
+    vector = Preprocessing.Transform_into_vector(voice1)
 
 
-for i in range(prediction_length):
-    length = len(voice1)
-    X = [voice1[length-window_size::]]
-    y_pred = reg.predict(scaler.transform(X))
-    y_pred = Postprocessing.prediction_vector_pick_highest(y_pred, zero_bias=0.1)[0]
+    # minimum note used for converting probability vector back into note
+    min_note = min(voice1[voice1>0])
+
+    predicted_voice = []
+
+    # Parameter to set length of prediction
+    prediction_length = 256
+
+    X, y = Preprocessing.Split_rolling_window(voice1, vector, window_size=window_size, train=False)
+
+    scaler = StandardScaler()
+    scaler.fit(X)
+    reg = LinearRegression().fit(scaler.transform(X), y)
 
 
-    vector = np.append(vector, y_pred)
+    for i in range(prediction_length):
+        length = len(voice1)
+        X = [voice1[length-window_size::]]
+        y_pred = reg.predict(scaler.transform(X))
+        y_pred = Postprocessing.prediction_vector_pick_highest(y_pred, zero_bias=0.1)[0]
 
-    # Converting vector to note number
-    note = Postprocessing.vector_to_note(min_note, y_pred)
 
-    predicted_voice.append(note)
-    voice1 = np.append(voice1, note)
+        vector = np.append(vector, y_pred)
 
-np.savetxt("output_complete.txt", voice1.astype(int), fmt='%s')
-np.savetxt("output_new.txt", predicted_voice, fmt='%s')
+        # Converting vector to note number
+        note = Postprocessing.vector_to_note(min_note, y_pred)
 
-Convert_to_MIDI.convert_to_midi("output_complete.txt", "output_complete.mid")
-Convert_to_MIDI.convert_to_midi("output_new.txt", "output_new.mid")
+        predicted_voice.append(note)
+        voice1 = np.append(voice1, note)
+
+    np.savetxt("output_complete.txt", voice1.astype(int), fmt='%s')
+    np.savetxt("output_new.txt", predicted_voice, fmt='%s')
+
+    Convert_to_MIDI.convert_to_midi("output_complete.txt", "output_complete.mid")
+    Convert_to_MIDI.convert_to_midi("output_new.txt", f"output_new{tracknumber}.mid")
+
+if __name__ == '__main__':
+    for i,wsize in zip([1,2,3,4],[670, 500, 500, 600]):
+        # Best so far: [650/670, 500, 680, 600]
+        create_track(i, wsize)
--- Code/linear_regression/Linear_regression_predict_all_voices.py
+++ Code/linear_regression/Linear_regression_predict_all_voices.py
@@ -2,25 +2,12 @@ import sys
 import os
 sys.path.append(os.path.abspath('../preprocessing'))
 sys.path.append(os.path.abspath('../postprocessing'))
+sys.path.append(os.path.abspath('../general'))
 import Preprocessing
 import Postprocessing
 from sklearn.linear_model import LinearRegression, Ridge
 import numpy as np
-from matplotlib import pyplot as plt
-from sklearn.metrics import accuracy_score, r2_score
-from sklearn.preprocessing import StandardScaler
-from multiprocessing import Pool, freeze_support
-
-
-def get_score(voice, vector, i):
-    X_train, X_test, y_train, y_test = Preprocessing.Split_rolling_window(voice, vector, window_size=i)
-
-    reg = LinearRegression().fit(X_train, y_train)
-
-    y_pred = reg.predict(X_test)
-    y_pred = Postprocessing.prediction_vector_pick_highest(y_pred, zero_bias=0.1)
-
-    return r2_score(y_test, y_pred)
+import Convert_to_MIDI
 
 if __name__ == '__main__':
     voices = np.loadtxt("../data/F.txt").T[:,3095::]
@@ -28,47 +15,69 @@ if __name__ == '__main__':
 
     # Here we are creating a new voice which is all voices combined after each other, so if we had [1,2] [3,4] [5,6]
     # and [7,8] and voices at first we now get [1, 3, 5, 7, 2, 4, 6, 8]
+
     for a, b, c, d in zip(voices[0],voices[1],voices[2],voices[3]):
         newvoice.append(a)
         newvoice.append(b)
         newvoice.append(c)
         newvoice.append(d)
 
+    newvoice = np.array(newvoice)
     vector = Preprocessing.Transform_into_vector(newvoice)
 
-    score = []
-    scorestd = []
-    difference = []
+    min_note = min(newvoice)
 
-    maxwindowsize = 700
-    window = range(4, maxwindowsize * 4, 4)
-    tot = len(window)
-    k = 0
+    lengths = [len(vector[i]) for i in range(4)]
 
-    # Multiprocessing
-    freeze_support()
-    pool = Pool()
+    # Outcome parameters
+    window_size = 2600
+    prediction_length = 256
+
+    # Training the model
+    X, y = Preprocessing.Split_rolling_window(newvoice, vector, window_size=window_size, train=False, output_size=4)
+    reg = LinearRegression().fit(X, y)
+
+    # New music generated
+    predicted_voice = []
+
+    for i in range(0,prediction_length,4):
+        length = len(newvoice)
+        X = [newvoice[length - window_size::]]
+
+        y_pred = reg.predict(X)[0]
+
+        # Transforming the vector
+        f = Postprocessing.prediction_vector_pick_highest
+        newvoice1 = f(y_pred[0:lengths[0]], zero_bias=0.1)
+        newvoice2 = f(y_pred[lengths[0]:lengths[1] + lengths[0]], zero_bias=0.1)
+        newvoice3 = f(y_pred[lengths[0] + lengths[1]:lengths[0] + lengths[1] + lengths[2]], zero_bias=0.1)
+        newvoice4 = f(y_pred[lengths[0] + lengths[1] + lengths[2]::], zero_bias=0.1)
+
+        for newnote in [newvoice1, newvoice2, newvoice3, newvoice4]:
+            note = Postprocessing.vector_to_note(min_note, newnote)
+            predicted_voice.append(note)
+            newvoice = np.append(newvoice, note)
 
 
-    for i in window:
-        for vector, voice in zip(vectors, voices):
-            tempscore = pool.starmap(get_score, [[voice, vector, i] for k in range(8)])
 
-            score.append(np.mean(tempscore))
-            scorestd.append(np.std(tempscore))
-            k += 1
-            print("Done:", k / tot)
+    # Now we have to transform the data back into 4 separate vectors
 
+    new = [[], [], [], []]
 
-    score = np.array(score)
-    scorestd = np.array(scorestd)
+    for i in range(int(len(predicted_voice)/4)):
+        new[0].append(predicted_voice[i])
+        new[1].append(predicted_voice[i+1])
+        new[2].append(predicted_voice[i+2])
+        new[3].append(predicted_voice[i+3])
+    old_and_new = [[], [], [], []]
+    for i in range(int(len(newvoice)/4)):
+        old_and_new[0].append(newvoice[i])
+        old_and_new[1].append(newvoice[i+1])
+        old_and_new[2].append(newvoice[i+2])
+        old_and_new[3].append(newvoice[i+3])
 
-    plt.plot(window, score)
-    plt.fill_between(window, score - scorestd, score + scorestd, alpha = 0.2)
-    plt.title(r"$R^2$ score of linear regression vs windows size")
-    plt.xlabel("Window size")
-    plt.ylabel(r"$R^2$ score")
-    plt.grid()
-    plt.savefig("regression_score_R2.pdf")
-    plt.show()
+    np.savetxt("4voices_output_complete.txt", np.array(old_and_new).T, fmt='%s')
+    np.savetxt("4voices_output_new.txt", np.array(new).T, fmt='%s')
 
+    Convert_to_MIDI.convert_to_midi("4voices_output_complete.txt", "4voices_output_complete.mid")
+    Convert_to_MIDI.convert_to_midi("4voices_output_new.txt", "4voices_output_new.mid")
